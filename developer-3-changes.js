cdcadfd950e50eb7b17678edda185173020ea4bd
khangaikhuu uvgunkhuu <khangaikhuu@gmail.com>
2022-12-22 13:38:13 +0800

    slides: update with oop

--- developer-3-changes.js
+++ developer-3-changes.js
@@ -1,1 +1,45 @@
 console.log("developer 3 changes");
+
+var o = {
+  name: "khangai",
+  call: function () {
+    console.log(`My name is ${this.name}`);
+  },
+};
+
+console.log(o.name);
+
+o.talk = function () {
+  console.log("wha");
+};
+
+console.log(o.talk());
+console.log(Math.sqrt(9));
+
+let now = new Date();
+console.log(now.getDate());
+class MyDate {
+  constructor() {
+    this._date = "2022-12-22";
+  }
+
+  getDate() {
+    return this._date;
+  }
+}
+
+class MyDateChild extends MyDate {
+  constructor() {
+    super();
+  }
+
+  setDate(a) {
+    this._date = a;
+  }
+}
+
+let nowMy = new MyDate(); // new object
+console.log(nowMy.getDate());
+
+let nowMy2 = new MyDateChild(); // new object
+nowMy2.setDate("2020202");
